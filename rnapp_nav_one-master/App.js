b14d3e67b8e5d75d16fe60012c116feee7d2dca3
yasirshakil <m.yasirshakil@gmail.com>
2019-04-20 11:37:16 -0700

    done

---
+++ rnapp_nav_one-master/App.js
@@ -0,0 +1,133 @@
+import React, { Component } from 'react';
+import { Text, View, Button, Image } from 'react-native';
+
+//Navigation Imports
+import { createStackNavigator, createAppContainer,  } from 'react-navigation';
+
+
+//Home Screen View Settings
+class Introduction   extends Component {
+  static navigationOptions = {
+    title: 'Introduction'
+    
+  }
+  render() {
+
+    return (
+      <View style={{ flex: 1, alignItems: "center", justifyContent: "center" }}>
+      
+      <Image
+          style={{width: 200, height: 200}}
+          source={{uri: 'https://cdn4.iconfinder.com/data/icons/web-ui-color/128/Account-512.png'}}
+        />
+
+        <Text>Introduction</Text>
+      
+        <Button
+          title="Education"
+          onPress={() => this.props.navigation.navigate('Education', {
+            itemId: 12,
+            otherMsg: "Sent to Education"
+          })}
+        />
+      </View>
+
+      
+
+    );
+  }
+}
+
+
+//Education Screen View Settings
+class EducationScreen extends Component {
+  static navigationOptions = {
+    title: 'Education Screen'
+  }
+  render() {
+    const { navigation } = this.props;
+    const itemId = navigation.getParam('itemId', 'NO-ID');
+    const otherMsg = navigation.getParam('otherMsg', 'This is the default Msg');
+
+    return (
+      <View style={{ flex: 1, alignItems: "center", justifyContent: "center" }}>
+        <Text> eductional background</Text>
+        
+        
+
+        <Button
+          title="Experience"
+          onPress={() => this.props.navigation.navigate('Experience')}
+        />
+        
+        <Button
+          title="Home"
+          onPress={() => this.props.navigation.navigate('Home')}
+        />
+
+        <Button
+          title="Go back"
+          onPress={() => this.props.navigation.goBack()}
+        />
+
+      </View>
+    );
+  }
+}
+
+
+
+
+
+//Experience Screen View Settings
+class ExperienceScreen extends Component {
+  static navigationOptions = {
+    title: 'Experience Screen'
+  }
+  render() {
+    const { navigation } = this.props;
+    const itemId = navigation.getParam('itemId', 'NO-ID');
+    const otherMsg = navigation.getParam('otherMsg', 'This is the default Msg');
+
+    return (
+      <View style={{ flex: 1, alignItems: "center", justifyContent: "center" }}>
+        <Text>Experience</Text>
+        
+        <Button
+          title="Home"
+          onPress={() => this.props.navigation.navigate('Home')}
+        />
+
+        <Button
+          title="Go back"
+          onPress={() => this.props.navigation.goBack()}
+        />
+
+      </View>
+    );
+  }
+}
+
+
+
+
+
+//Creation of the Stack Navigator
+const AppNavigator = createStackNavigator({
+  Home: Introduction,
+  Education: EducationScreen,
+  Experience: ExperienceScreen
+},
+  {
+    initialRouteName: "Home"
+  });
+
+//Exporting while creating the App Container
+// export default createAppContainer(AppNavigator);
+const AppContainer = createAppContainer(AppNavigator);
+export default class App extends Component {
+  render() {
+    return <AppContainer />
+  }
+}
+
